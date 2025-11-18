7c0d453bb49ee81d842b1df583d8066df5d43b3b
tbrekalo <t@tbrekalo.xyz>
2025-11-18 23:49:36 +0000

    master: working pull json parser

---
+++ example.cc
@@ -0,0 +1,70 @@
+#include "tbrekalo/json.hpp"
+#include <print>
+#include <vector>
+
+static constexpr std::string_view SRC{
+R"(
+{
+  "foo": 42,
+  "bar": null,
+  "baz": [
+    1,
+    2.0,
+    -3,
+    0,
+    -3.0E+2,
+    3.0E+2
+  ],
+  "buz": [
+    {
+      "qux": {
+        "corge": [
+          "null"
+        ]
+      }
+    }
+  ],
+  "42": true,
+  "": false
+}
+)"};
+
+auto main(int, char**) -> int {
+  namespace tb = tbrekalo;
+
+  std::print("{}\n", SRC);
+  tb::json::parser parser(SRC);
+
+  parser.pull_object([&](std::string_view key) {
+    if (key == "foo") { std::print("{}: {}\n", key, *parser.pull_number()); }
+    if (key == "bar") { std::print("{}: {}\n", key, *parser.pull_null()); }
+    if (key == "baz") {
+      std::vector<std::string_view> nums;
+      parser.pull_list([&] {
+        if (auto number = parser.pull_number(); number) { nums.push_back(*number); }
+      });
+
+      std::print("{}: {}\n", key, nums);
+    }
+    if (key == "buz") {
+      parser.pull_list([&] {
+        parser.pull_object([&](std::string_view key) {
+          if (key == "qux") {
+            parser.pull_object([&](std::string_view key) {
+              std::vector<std::string_view> vec;
+
+              if (key == "corge") {
+                parser.pull_list([&] { vec.push_back(*parser.pull_string()); });
+                std::print("buz:qux:corge: {}\n", vec);
+              }
+            });
+          }
+        });
+      });
+    }
+    if (key == "42") { std::print("42: {}\n", *parser.pull_bollean()); }
+    if (key == "") { std::print("\"\": {}\n", *parser.pull_bollean()); }
+  });
+
+  return EXIT_SUCCESS;
+}
---
+++ include/tbrekalo/json.hpp
@@ -0,0 +1,124 @@
+#ifndef TBREKALO_JSON_HPP_
+#define TBREKALO_JSON_HPP_
+
+#include <cwctype>
+#include <optional>
+#include <string_view>
+
+namespace tbrekalo::json {
+
+class parser {
+  bool error_{false};
+  char const* first_;
+  char const* last_;
+
+  auto skip_while(auto&& predicate, ptrdiff_t offset) noexcept -> bool {
+    for (first_ += offset; !error_ && first_ < last_ && predicate(*first_); ++first_);
+    return !(error_ |= !(first_ <= last_));
+  }
+
+  template <class Predicate>
+  auto skip_except(Predicate&& predicate, std::string_view set, ptrdiff_t offset = 0) noexcept -> bool {
+    return skip_while(std::forward<Predicate>(predicate), offset) && !(error_ |= !(set.contains(*first_)));
+  }
+
+  static constexpr auto (*pred_wspace)(char) noexcept
+      -> bool = [] [[gnu::always_inline]] (char c) noexcept -> bool { return std::isspace(c); };
+  static constexpr auto (*pred_digits)(char) noexcept
+      -> bool = [] [[gnu::always_inline]] (char c) noexcept -> bool { return c != ','; };
+
+  static constexpr std::string_view VALID_ESCAPES{R"("\/bfnrt)"};
+  static constexpr std::string_view FALSE_STR{"false"};
+  static constexpr std::string_view TRUE_STR{"true"};
+  static constexpr std::string_view NULL_STR{"null"};
+
+  template <auto value> auto try_pull_value_str(std::string_view target, bool set_error) noexcept
+      -> std::optional<decltype(value)> {
+    if (error_ || last_ - first_ < std::ssize(target) || target != std::string_view(first_, target.size())) {
+      error_ |= set_error;
+      return std::nullopt;
+    }
+    first_ += target.length();
+    return value;
+  }
+
+public:
+  explicit parser(std::string_view src) : first_(src.begin()), last_(src.end()) {}
+
+  auto halt() noexcept -> void { error_ |= true; }
+  auto error() const noexcept -> bool { return error_; }
+
+  auto pull_null() noexcept -> std::optional<std::nullptr_t> { return try_pull_value_str<nullptr>(NULL_STR, true); }
+
+  auto pull_bollean() noexcept -> std::optional<bool> {
+    return try_pull_value_str<false>(FALSE_STR, false).or_else([&] {
+      return try_pull_value_str<true>(TRUE_STR, true);
+    });
+  }
+
+  auto pull_string() noexcept -> std::optional<std::string_view> {
+    if ((error_ |= *first_ != '"')) { return std::nullopt; }
+
+    bool escaped = false;
+    for (char const* begin = first_++; first_ < last_; ++first_) {
+      if (escaped) {
+        escaped = false;
+        if ((error_ |= !VALID_ESCAPES.contains((*first_)))) { return std::nullopt; }
+      } else if (*first_ == '\\') {
+        escaped = true;
+      } else if (*first_ == '"') {
+        return std::string_view(begin + 1, first_++);
+      }
+    }
+    return std::nullopt;
+  }
+
+  auto pull_number() noexcept -> std::optional<std::string_view> {
+    auto const* begin = first_;
+    if (first_ < last_ && *first_ == '-') { ++first_; }
+    if ((error_ |= !(first_ < last_ && std::isdigit(*first_)))) { return std::nullopt; }
+    for (; first_ < last_ && std::isdigit(*first_); ++first_);
+
+    if (first_ < last_ && *first_ == '.') {
+      auto n_digits = 0;
+      for (++first_; first_ < last_ && std::isdigit(*first_); ++first_, ++n_digits);
+      if ((error_ |= n_digits == 0)) { return std::nullopt; }
+    };
+
+    if (first_ < last_ && (*first_ == 'e' || *first_ == 'E')) {
+      auto n_digits = 0;
+      if (++first_ < last_ && (*first_ == '-' || *first_ == '+')) { ++first_; }
+      for (; first_ < last_ && std::isdigit(*first_); ++first_, ++n_digits);
+      if ((error_ |= n_digits == 0)) { return std::nullopt; }
+    }
+    return std::string_view(begin, first_);
+  }
+
+  auto pull_list(auto&& visitor) -> bool {
+    if (error_ || !skip_except(pred_wspace, "[")) { return !(error_ |= true); }
+    for (skip_while(pred_wspace, 1); !error_ && first_ < last_ && *first_ != ']';
+         skip_except(pred_wspace, ",]") && skip_while(pred_wspace, *first_ == ',')) {
+      if (!skip_while(pred_wspace, *first_ == ',')) { return false; }
+      visitor();
+    }
+    if (first_ >= last_ || *first_++ != ']') { return !(error_ |= true); }
+    return !error_;
+  }
+
+  auto pull_object(auto&& visitor) -> bool {
+    if (error_ || !skip_except(pred_wspace, "{")) { return !(error_ |= true); }
+    for (skip_while(pred_wspace, 1); !error_ && first_ < last_ && *first_ != '}';
+         skip_except(pred_wspace, ",}") && skip_while(pred_wspace, *first_ == ',')) {
+      if (auto key = pull_string(); key && skip_except(pred_wspace, ":") && skip_while(pred_wspace, 1)) {
+        visitor(*key);
+      }
+    }
+
+    if (first_ >= last_ || *first_++ != '}') { return !(error_ |= true); }
+    return !error_;
+  }
+};
+
+} // namespace tbrekalo::json
+
+#endif /* TBREKALO_JSON_HPP_ */
